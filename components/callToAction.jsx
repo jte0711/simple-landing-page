b4aaa6c8452bff09a6154a534a294ce8d7a48eae
James Tedy <james.tedy@ymail.com>
2020-01-08 17:40:29 +1100

    Add mobile view to calltoaction

--- components/callToAction.jsx
+++ components/callToAction.jsx
@@ -10,15 +10,58 @@ let headStyle = {
   marginTop: "3rem"
 }
 
+let featuresStyleMb = {
+  marginTop: "2rem",
+  marginBottom: "3rem"
+};
+
+let headStyleMb = {
+  marginTop: "0.5rem"
+}
+
 class CallToAction extends React.Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      screenWidth: 1000,
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+
+  resize(){
+    let curWidth = window.innerWidth;
+    if (curWidth !== this.state.screenWidth) {
+        this.setState({screenWidth:curWidth});
+    }
+  }
+
   render(){
-    return(
-      <div className="container-fluid d-flex flex-column align-items-center" style={featuresStyle}>
-        <hr className="hrStyle"/>
-        <h1 className="h1Style" align="center" style={headStyle}>One app to manage all. <br/> Join us now.</h1>
-        <Buttons addClass="buttonCta homeButton-L" link="/signup" text="Get started"/>
-      </div>
-    );
+
+    let isMobile = (this.state.screenWidth <= 720);
+
+    if (isMobile !== true){
+      return(
+        <div className="container-fluid d-flex flex-column align-items-center" style={featuresStyle}>
+          <hr className="hrStyle"/>
+          <h1 className="h1Style" align="center" style={headStyle}>One app to manage all. <br/> Join us now.</h1>
+          <Buttons addClass="buttonCta homeButton-L" link="/signup" text="Get started"/>
+        </div>
+      );
+    } else {
+      return(
+        <div className="container-fluid d-flex flex-column align-items-center" style={featuresStyleMb}>
+          <hr className="hrStyle"/>
+          <h1 className="h1Style" align="center" style={headStyleMb}>One app to manage all. <br/> Join us now.</h1>
+          <Buttons addClass="buttonCta homeButton-L" link="/signup" text="Get started"/>
+        </div>
+      );
+    }
+    
   }
 }
 
